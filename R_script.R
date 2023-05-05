776cbb715f1d414871429c8cf068e68f91d7e0c8
RINKU AHIRWAR <99554758+RinkuAhirwar@users.noreply.github.com>
2023-05-05 19:10:38 +0530

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -8,7 +8,7 @@ library(rgl)
 library(MetBrewer)
 library(colorspace)
 library(rayrender)
-data <- st_read("C://Users//Rahul Ahirwar//Desktop//random_thoughts//maps//population_map//data//kontur_population_IN_20220630.gpkg")
+data <- st_read("kontur_population_IN_20220630.gpkg")
 crsLAEA <- "+proj=lcc +lat_0=24 +lon_0=80 +lat_1=12.472955 +lat_2=35.1728044444444 +x_0=4000000 +y_0=4000000"
 projected <- st_transform(data,crsLAEA)
 
@@ -68,7 +68,7 @@ mat |>
           background="#FBE3C2")
 render_camera(theta = -25,phi=60,zoom=.9)
 
-rgl.snapshot("C://Users//Rahul Ahirwar//Desktop//random_thoughts//maps//population_map//images//population_map.png",fmt="png")
+rgl.snapshot("population_map.png",fmt="png")
 
 
 
