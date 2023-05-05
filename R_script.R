60f3a92d1f878e463109f7605783631779f77095
RINKU AHIRWAR <99554758+RinkuAhirwar@users.noreply.github.com>
2023-05-05 19:09:56 +0530

    Add files via upload

--- R_script.R
+++ R_script.R
@@ -8,15 +8,13 @@ library(rgl)
 library(MetBrewer)
 library(colorspace)
 library(rayrender)
-data <- st_read("kontur_population_IN_20220630.gpkg")
-
-
+data <- st_read("C://Users//Rahul Ahirwar//Desktop//random_thoughts//maps//population_map//data//kontur_population_IN_20220630.gpkg")
 crsLAEA <- "+proj=lcc +lat_0=24 +lon_0=80 +lat_1=12.472955 +lat_2=35.1728044444444 +x_0=4000000 +y_0=4000000"
 projected <- st_transform(data,crsLAEA)
 
 head(projected)
 
-bb <- st_bbox(data)
+bb <- st_bbox(projected)
 
 height <- st_distance(
   st_point(c(bb[["xmin"]], bb[["ymin"]])),
@@ -38,9 +36,9 @@ if (height > width) {
 height_ratio
 width_ratio
 
-size <- 3000
+size <- 1000
 
-rast_ <- st_rasterize(data,nx=floor(size*width_ratio),
+rast_ <- st_rasterize(projected,nx=floor(size*width_ratio),
                       ny=floor(size*height_ratio))
 
 mat <- matrix(rast_$population,
@@ -49,23 +47,43 @@ mat <- matrix(rast_$population,
 
 
 c1 <- met.brewer("Hokusai2")
-
-texture <- grDevices::colorRampPalette(c1,bias=2)(256)
-swatchplot(texture)
+c2<-met.brewer("OKeeffe2")
+print(c2)
+print(colorRampPalette(c2,bias=0.1)(256))
+texture_ <- grDevices::colorRampPalette(c1,bias=4)(256)
+swatchplot(texture_)
+print(texture_)
 
 rgl.close()
 
+rgl.clear()
+
 mat |>
-  height_shade(texture = texture) |>
+  height_shade(texture = texture_) |>
   plot_3d(heightmap = mat ,
           zscale=100,
           solid=FALSE,
           shadowdepth = 0,
-          shadow_darkness=.95)
+          shadow_darkness=.95,
+          background="#FBE3C2")
 render_camera(theta = -25,phi=60,zoom=.9)
 
+rgl.snapshot("C://Users//Rahul Ahirwar//Desktop//random_thoughts//maps//population_map//images//population_map.png",fmt="png")
 
 
-render_highquality(filename="draft1.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+rgl.close()
 
 
